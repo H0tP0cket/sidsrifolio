ba9b2fb528ac99eb19fda668533d99343b502afb
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2022-05-16 09:50:26 -0700

    i did something idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 py-2 ">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-800 py-2 ">
       <Head>
         <title>Siddharth Srinivasan </title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,21 +13,22 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col  pt-16 px-16">
         <div className="text-4xl font-bold text-teal-400">
-          <Typewriter words={["Hey I'm Sid Sri", "A Web Developer"]}
-            loop={10000}
+          <h1>Hey I'm Sid Sri</h1>
+          <Typewriter words={[ "A Web Developer"]}
+            
             cursor
             cursorStyle='|'
             typeSpeed={70}
-            deleteSpeed={50}
+            
             delaySpeed={1000} />
         </div>
       </main>
-
+      <hr></hr>
       <footer className="flex h-24 w-full items-center pl-16 text-white">
-        <hr/>
+        
         <div>
-          Copyright ©2022{' '}
-          <a href="https://github.com/H0tP0cket">Siddharth Srinivasan'22</a> All
+          Copyright ©2022
+          <a href="https://github.com/H0tP0cket">Siddharth Srinivasan</a> All
           Rights Reserved.
         </div>
       </footer>
