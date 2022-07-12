993508b5ea7f4b205ff6ebffbc1d97337dc88a9e
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2022-07-12 03:13:32 -0700

    halal dropdown menu

--- pages/dropdown.tsx
+++ pages/dropdown.tsx
@@ -26,45 +26,23 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 py-2 ">
 
-      <Head>
-        <title>Siddharth Srinivasan </title>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="preload"
-          href="/fonts/modern.ttf"
-          as="font"
-          crossOrigin=""
-        />
-      </Head>
-      { loading ? (
-        <>
-        <div className = "pb-12">
-      <PacmanLoader color={"#08fdd8"} loading={loading}  size={100} />
-      </div>
-      <div className="text-4xl font-bold text-teal-400 pt-20 pl-16">
-          
-          <Typewriter
-            words={['loading...']}
-            cursor
-            cursorStyle="|"
-            typeSpeed={70}
-            deleteSpeed={100}
-            delaySpeed={1000}
-          />
-        </div>
-      </>
+      
+      
 
-    ) : (
-
-    <>
+    
       <Navbar />
       <main className="flex w-full flex-1 flex-col  px-16 pt-24">
-        
+        <nav className = "text-4xl text-white ">
+            <a href = "#">HOME</a>
+            <a href = "#">ABOUT</a>
+            <a href = "#">PROJECTS</a>
+            <a href = "#">CONTACT</a>
+        </nav>
       </main>
 
       <Footer/>
-      </>
-)}
+      
+
     </div> 
     
   
