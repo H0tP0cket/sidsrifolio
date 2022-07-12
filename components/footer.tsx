a41db3f8563d7675d7a8ba65869c84f2b5e86c83
Siddharth Srinivasan <77374762+H0tP0cket@users.noreply.github.com>
2022-07-12 01:18:10 -0700

    halal

---
+++ components/footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Footer() {
+  return (
+    <footer>
+      <div className="left-0 right-0 mx-auto w-full max-w-6xl  bg-gray-900 px-6 sm:px-6">
+        <div className="py-4 md:flex md:items-center md:justify-between md:py-8 ">
+          
+            
+    
+          <div className="mr-4 text-md text-white">
+            Designed & Built by Siddharth Srinivasan
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,50 +2,81 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import * as Logo from 'public/SLogo.png'
+
+import Link from 'next/link'
+import Slogo from '../public/halalimage.png'
+import Image from 'next/image'
+import { motion } from 'framer-motion'
+
+
+
 function Navbar() {
   const [top, setTop] = useState(true)
+  const [open, setOpen] = useState(false)
   useEffect(() => {
     const scrollHandler = () => {
-      window.pageYOffset > 10 ? setTop(false) : setTop(true)
+      window.pageYOffset > 14 ? setTop(false) : setTop(true)
     }
     window.addEventListener('scroll', scrollHandler)
     return () => window.removeEventListener('scroll', scrollHandler)
   }, [top])
   return (
-    <div
-      className={` mt-30 fixed top-0 flex h-24 w-full items-center  bg-gray-900 transition duration-300 ease-in-out md:bg-opacity-90 ${
+    <nav
+      className={` fixed top-0 z-10 flex h-24 w-full items-center   transition duration-300 ease-in-out md:bg-opacity-90 ${
         !top && 'bg-gray-850 shadow-lg backdrop-blur'
       }`}
     >
-      <a className="block" aria-label="Logo">
-        <img src="public/Slogo1.png" width="40px" alt="Siddharth Logo" />
-      </a>
-      <a
-        href="#based"
-        className="text-l  ml-24 cursor-pointer items-center justify-center text-teal-400 opacity-100 transition duration-300 ease-in-out hover:text-white"
-      >
-        About
-      </a>
-      <a
-        href="#superbased"
-        className="text-l ml-12  cursor-pointer items-center justify-center text-teal-400 opacity-100 transition duration-300 ease-in-out hover:text-white"
-      >
-        Skills
-      </a>
-      <a
-        href="#extrabased"
-        className="text-l ml-12  cursor-pointer items-center justify-center text-teal-400 opacity-100 transition duration-300  ease-in-out hover:text-white"
-      >
-        Experience
-      </a>
-      <a
-        href="#extremelybased"
-        className="text-l ml-12  cursor-pointer items-center justify-center text-teal-400 opacity-100 transition duration-300 ease-in-out hover:text-white"
-      >
-        Contact
-      </a>
-    </div>
+      <div className="relative flex w-full justify-between">
+        {/* <div className="w-full flex-grow flex-row justify-between md:w-auto mr-4"> !!!!!! mr-4 */}
+        {/* Logo */}
+        <Link href="/">
+          <a className="block pl-8" aria-label="">
+            <Image src={Slogo} width="96px" height="68px" alt="Logo" />
+          </a>
+        </Link>
+        
+        <button
+          onClick={() => setOpen(!open)}
+          className="leading-none focus:outline-none pr-20 "
+          aria-label="Navbar Toggle"
+        >
+          <svg
+           
+            className={
+              'text-white ' +
+              (open
+                ? ' hidden'
+                : ' animate__animated animate__lightSpeedInLeft animate__faster')
+            }
+            xmlns="http://www.w3.org/2000/svg"
+            width="64"
+            height="64"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+          <svg
+                className={
+                  "text-white    " +
+                  (!open
+                    ? " hidden"
+                    : " animate__animated animate__lightSpeedInRight animate__faster ")
+                }
+                xmlns="http://www.w3.org/2000/svg"
+                width="64"
+                height="64"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+        </button>
+      </div>
+    </nav>
   )
 }
 
